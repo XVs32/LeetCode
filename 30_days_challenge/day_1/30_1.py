0a6d59bc75e9c690564753214812b4c778da4b17
marco <makaut1995.cs02@g2.nctu.edu.tw>
2020-04-02 04:20:41 +0800

    day 1 of 30 challenge Xor sol

--- 30_days_challenge/day_1/30_1.py
+++ 30_days_challenge/day_1/30_1.py
@@ -2,21 +2,14 @@ class Solution:
 
     def singleNumber(self, nums):
         
-        table = {}
-        
-        for i in range(len(nums)):
-            
-            if not table.__contains__(nums[i]):
-                table[nums[i]] = 1;
-            else:
-                table.pop(nums[i])
-    
-        return list(table)[0]
-    
+        ans = 0
+        for i in nums:
+            ans = ans ^ i
+        return ans
         
         
 a = Solution()
 print("%s" % a.singleNumber([2,2,1]))
 print("%s" % a.singleNumber([4,1,2,1,2]))
-print("%s" % a.singleNumber([4,1,2,1,2]))
+print("%s" % a.singleNumber([-1]))
 
