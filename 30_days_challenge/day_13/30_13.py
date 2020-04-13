d15473b7b9b9f2e96a8198e1f3d685bc54cdb54e
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2020-04-13 19:23:38 +0800

    day_11 without md

---
+++ 30_days_challenge/day_13/30_13.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+    
+    
+    
+a = Solution()
+
+print (a.findMaxLength([1,2,3]))
+print (a.findMaxLength([1,1,3,3,5,5,7,7]))
+print (a.findMaxLength([1,3,2,3,5,0]))
+print (a.findMaxLength([1,1,2,2]))
+print (a.findMaxLength([2,9,0,7,6,2,7,7,0]))
