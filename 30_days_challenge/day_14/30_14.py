1bfddb2e0e684f2cfebb48be96a0a91191c28da1
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2020-04-30 02:59:01 +0800

    day 14 fix

--- 30_days_challenge/day_14/30_14.py
+++ 30_days_challenge/day_14/30_14.py
@@ -1,38 +1,17 @@
 class Solution:
-    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+    def stringShift(self, s, shift):
         
-        prefix_sum = {}
-        prefix_sum[50000] = [-1,-1]
-        ones = 0
-        zeros = 0
-        length = len(nums)
-        i = 0
-        while i < length:
+        count = 0
+        
+        for op in shift:
             
-            ones += nums[i] & 1
-            zeros += ~nums[i] & 1
-            
-            diff = ones - zeros + 50000
-            
-            if not prefix_sum.__contains__(diff):
-                prefix_sum[diff] = [i,i]
+            if op[0] == 0:
+                count += op[1]
             else:
-                prefix_sum[diff][0] = min(prefix_sum[diff][0],i)
-                prefix_sum[diff][1] = max(prefix_sum[diff][1],i)
+                count -= op[1]
             
-            
-            i += 1
+        count %= len(s)
         
-        ans = 0
-        for perfix in prefix_sum.values():
-            ans = max(perfix[1] - perfix[0],ans)
-            
-        return ans
-    
-a = Solution()
-
-print (a.findMaxLength([0,1]))
-print (a.findMaxLength([0,1,0]))
-print (a.findMaxLength([]))
-print (a.findMaxLength([0,0,0,0]))
-print (a.findMaxLength([0,1,1,1,1,0,0]))
+        s = s[count:] + s[:count]
+        
+        return s
